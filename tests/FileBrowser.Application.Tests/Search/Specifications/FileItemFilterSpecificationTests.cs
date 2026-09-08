bcbc3e047db0d8816f451356b944a1b35d3f6358
Nicholas Ibarra <nlibarra12@gmail.com>
2026-09-07 22:13:22 -0500

    feat: added spec

---
+++ tests/FileBrowser.Application.Tests/Search/Specifications/FileItemFilterSpecificationTests.cs
@@ -0,0 +1,57 @@
+﻿using FileBrowser.Application.Abtractstions.Indexing;
+using FileBrowser.Application.Search;
+using FileBrowser.Application.Search.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileBrowser.Application.Tests.Search.Specifications;
+
+public class FileItemFilterSpecificationTests
+{
+    [Fact]
+    public void Criteria_WhenSearchItemTypeIsNull_ShouldMatchFilesAndDirectories()
+    {
+        var specification = new FileItemFilterSpecification(null);
+        var criteria = specification.Criteria.Compile();
+
+        Assert.True(criteria(CreateEntry(isDirectory: false)));
+        Assert.True(criteria(CreateEntry(isDirectory: true)));
+    }
+
+    [Fact]
+    public void Criteria_WhenSearchItemTypeIsFile_ShouldMatchOnlyFiles()
+    {
+        var specification =
+            new FileItemFilterSpecification(SearchItemType.File);
+
+        var criteria = specification.Criteria.Compile();
+
+        Assert.True(criteria(CreateEntry(isDirectory: false)));
+        Assert.False(criteria(CreateEntry(isDirectory: true)));
+    }
+
+    [Fact]
+    public void Criteria_WhenSearchItemTypeIsDirectory_ShouldMatchOnlyDirectories()
+    {
+        var specification =
+            new FileItemFilterSpecification(SearchItemType.Directory);
+
+        var criteria = specification.Criteria.Compile();
+
+        Assert.True(criteria(CreateEntry(isDirectory: true)));
+        Assert.False(criteria(CreateEntry(isDirectory: false)));
+    }
+
+    private FileIndexEntry CreateEntry(bool isDirectory)
+    {
+        return new FileIndexEntry(
+            "test",
+            "test",
+            "test",
+            isDirectory,
+            "Test",
+            null, null,
+            default);
+    }
+}
